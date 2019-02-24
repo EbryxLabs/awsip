e4969697de2e8f1c1dbd6d78274f1d20973f1cf3
Rana Awais <rana.awais@ebryx.com>
2019-02-24 18:56:33 +0500

    script to check ip status.

---
+++ awsip/main.py
@@ -0,0 +1,83 @@
+import os
+import json
+import logging
+import argparse
+import ipaddress
+
+
+import requests
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handle = logging.StreamHandler()
+handle.setLevel(logging.INFO)
+handle.setFormatter(logging.Formatter('%(asctime)s: %(message)s'))
+logger.addHandler(handle)
+
+
+RANGE_URL = 'https://ip-ranges.amazonaws.com/ip-ranges.json'
+
+
+def get_params():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-ips', nargs='*',
+                        help='white spaced list of ip addresses / ip ranges. '
+                             'AND/OR filename containing ip address / range'
+                             'at each line.')
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+
+    logger.info(str())
+    params = get_params()
+    ips = params.ips
+    if not ips:
+        logger.info('No ips detected.')
+        exit(0)
+
+    for ip in ips.copy():
+        if os.path.isfile(ip):
+            ips.remove(ip)
+            ips.extend([x.strip('\n') for x in open(ip, 'r').readlines()])
+
+    ips = list(set(ips))
+    res = requests.get(RANGE_URL)
+    res = json.loads(res.content)
+    if 'prefixes' not in res:
+        logger.info('Could not get ip prefixes from url: %s', RANGE_URL)
+        exit(0)
+
+    ipdata = res['prefixes']
+    for ip in ips:
+
+        ips_to_check = list()
+        if '/' in ip:
+            ips_to_check.append(ipaddress.ip_network(ip)[0])
+            ips_to_check.append(ipaddress.ip_network(ip)[-1])
+        else:
+            ips_to_check.append(ip)
+
+        for data in ipdata:
+            within_range = list()
+            iprange = data['ip_prefix'] if 'ip_prefix' in \
+                data else data['ipv6_prefix']
+
+            for target_ip in ips_to_check:
+                if ipaddress.ip_address(target_ip) in \
+                        ipaddress.ip_network(iprange):
+                    within_range.append(True)
+                else:
+                    within_range.append(False)
+
+            if False not in within_range:
+                logger.info('%s belongs to [%s][%s][%s]',
+                            ip, iprange, data['region'], data['service'])
+
+
+if __name__ == "__main__":
+    main()
---
+++ awsip/setup.py
@@ -0,0 +1,32 @@
+import re
+import setuptools
+
+
+version = re.search(
+    r'^__version__\s*=\s*"(.*)"',
+    open('__init__.py').read(), re.M).group(1)
+
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='awsip',
+    version=version,
+    author='Rana Awais',
+    author_email='rana.awais@ebryx.com',
+    description='A simple package to check if ip address / range '
+                'belongs to AWS.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/pypa/sampleproject',
+    packages=setuptools.find_packages(),
+    install_requires=['requests'],
+    entry_points={'console_scripts': ['awsip = main:main']},
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
